35ad6f649d1f04ff7392fed0a4821a37b0d119c8
Abel de la Rosa <abel.delarosa@bairesdev.com>
2024-05-05 22:22:38 -0400

    Add card model

---
+++ RapidPay/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿namespace RapidPay.Models
+{
+    public class Card
+    {
+        public Card()
+        {
+            
+        }
+        public int Id { get; set; }
+        public string CardNumber { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
