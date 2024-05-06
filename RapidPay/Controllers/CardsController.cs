240e5068e6f6faa1fd094a22c5ce5e21e9f91ca8
Abel de la Rosa <abel.delarosa@bairesdev.com>
2024-05-05 22:13:32 -0400

    Add cards controller

---
+++ RapidPay/Controllers/CardsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RapidPay.Services;
+
+namespace RapidPay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        private readonly ICardService _cardService;
+
+        public CardsController(ICardService cardService)
+        {
+            _cardService = cardService;
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateCard()
+        {
+            string cardNumber = await _cardService.CreateCardAsync();
+            return Ok(cardNumber);
+        }
+
+        [HttpPost("pay")]
+        public async Task<IActionResult> Pay(string cardNumber, decimal amount)
+        {
+            bool success = await _cardService.PayAsync(cardNumber, amount);
+            if (success)
+                return Ok("Payment suceessful");
+            else
+                return BadRequest("Invalid card number");
+        }
+
+        [HttpGet("balance")]
+        public async Task<IActionResult> GetCardBalance(string cardNumber)
+        {
+            decimal balance = await _cardService.GetCardBalanceAsync(cardNumber);
+            return Ok(balance);
+        }
+    }
+}
