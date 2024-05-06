80969e83176dc34ebdf12cf5a018620f95bd1286
Abel de la Rosa <abel.delarosa@bairesdev.com>
2024-05-05 22:22:52 -0400

    Add Async repository interface

---
+++ RapidPay/Persistence/IAsyncRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+
+namespace RapidPay.Persistence
+{
+    public interface IAsyncRepository<T> where T : new()
+    {
+
+        Task<T> GetById(int id);
+        Task<T> FirstOrDefault(Expression<Func<T, bool>> predicate);
+
+        Task Add(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+
+        Task<IEnumerable<T>> GetAll();
+        Task<IEnumerable<T>> GetWhere(Expression<Func<T, bool>> predicate);
+
+        Task<int> CountAll();
+        Task<int> CountWhere(Expression<Func<T, bool>> predicate);
+
+    }
+}
